211e53bc8639993eec72aa737064d022c6d716e7
yohagos <yosef.hagos@googlemail.com>
2025-10-29 15:36:45 +0100

    adding channels, buffering channels and channels synchronization

---
+++ concepts/channels.go
@@ -0,0 +1,24 @@
+package concepts
+
+import "fmt"
+
+// Channels are the pipes that connect concurrent goroutines. 
+// You can send values into channels from one goroutine and 
+// receive those values into another goroutine.
+
+func Channels() {
+	// Create a new channel with make(chan val-type). 
+	// Channels are typed by the values they convey.
+	messages := make(chan string)
+
+	// Send a value into a channel using the channel <- syntax. 
+	// Here we send "ping" to the messages channel we made above, 
+	// from a new goroutine.
+	go func() { messages <- "ping" }()
+
+	// The <-channel syntax receives a value from the channel. 
+	// Here we’ll receive the "ping" message we sent above and 
+	// print it out.
+	msg := <-messages
+	fmt.Println(msg)
+}
