8cd4ac0ca39856a01285cfc1c91806ad973dc945
yohagos <yosef.hagos@googlemail.com>
2025-10-29 19:41:29 +0100

    adding atomic counters and rate limitings

---
+++ concepts/atomic-counters.go
@@ -0,0 +1,39 @@
+package concepts
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+// The primary mechanism for managing state in Go is communication 
+// over channels. We saw this for example with worker pools. There 
+// are a few other options for managing state though. Here we’ll 
+// look at using the sync/atomic package for atomic counters accessed 
+// by multiple goroutines.
+
+func AutomicCounters() {
+	// We’ll use an atomic integer type to represent our (always-positive) counter.
+	var ops atomic.Uint64
+
+	// A WaitGroup will help us wait for all goroutines to finish their work.
+	var wg sync.WaitGroup
+
+	// We’ll start 50 goroutines that each increment the counter exactly 1000 times.
+	for range 50 {
+		wg.Go(func() {
+			for range 1000 {
+				// To atomically increment the counter we use Add.
+				ops.Add(1)
+			}
+		})
+	}
+
+	// Wait until all the goroutines are done
+	wg.Wait()
+
+	// Here no goroutines are writing to ‘ops’, but using Load it’s 
+	// safe to atomically read a value even while other goroutines 
+	// are (atomically) updating it.
+	fmt.Println("ops => ", ops.Load())
+}
