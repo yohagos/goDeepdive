c0c02965f081c5b25e067c77c5212d56c6d29682
yohagos <yosef.hagos@googlemail.com>
2025-10-31 16:37:09 +0100

    adding sha and base64 encoding

---
+++ conceptstwo/sha-hashes.go
@@ -0,0 +1,27 @@
+package conceptstwo
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+// SHA256 hashes are frequently used to compute short identities for binary or text blobs. For example, TLS/SSL certificates use SHA256 to compute a certificate’s signature. Here’s how to compute SHA256 hashes in Go.
+
+func ShaHashes() {
+	s := "sha256 this string"
+
+	// Here we start with a new hash.
+
+	h := sha256.New()
+
+	// Write expects bytes. If you have a string s, use []byte(s) to coerce it to bytes.
+
+	h.Write([]byte(s))
+
+	// This gets the finalized hash result as a byte slice. The argument to Sum can be used to append to an existing byte slice: it usually isn’t needed.
+
+	bs := h.Sum(nil)
+	fmt.Println(s)
+	fmt.Printf("%x\n", bs)
+
+}
