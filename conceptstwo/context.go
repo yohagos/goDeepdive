11f906b2512c484d82d4a177d031082cdbbef1b7
yohagos <yosef.hagos@googlemail.com>
2025-10-31 16:41:02 +0100

    adding context, defers, json, line filters, reading/writing files and regex

---
+++ conceptstwo/context.go
@@ -0,0 +1,39 @@
+package conceptstwo
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func hello2(w http.ResponseWriter, req *http.Request) {
+
+	// A context.Context is created for each request by the net/http machinery, and is available with the Context() method.
+
+	ctx := req.Context()
+	fmt.Println("server: hello handler started")
+	defer fmt.Println("server: hello handler ended")
+
+	// Wait for a few seconds before sending a reply to the client. This could simulate some work the server is doing. While working, keep an eye on the context’s Done() channel for a signal that we should cancel the work and return as soon as possible.
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "hello\n")
+	case <-ctx.Done():
+
+		// The context’s Err() method returns an error that explains why the Done() channel was closed.
+
+		err := ctx.Err()
+		fmt.Println("server:", err)
+		internalError := http.StatusInternalServerError
+		http.Error(w, err.Error(), internalError)
+	}
+}
+
+func Context() {
+
+	// As before, we register our handler on the “/hello” route, and start serving.
+
+	http.HandleFunc("/hello", hello2)
+	http.ListenAndServe(":8090", nil)
+}
